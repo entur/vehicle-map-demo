5e3b786a2286328f04d4248110f742e2e2d41f8d
Tom Erik Støwer <testower@gmail.com>
2020-12-07 15:43:49 +0100

    Hydrate data

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
 import React, { useEffect, useReducer } from 'react';
 import './App.css';
 import Map from './Map';
-import { gql, useSubscription } from '@apollo/client';
+import { gql, useQuery, useSubscription } from '@apollo/client';
+
+const VEHICLE_FRAGMET = gql`
+  fragment VehicleFragment on VehicleUpdate {
+    codespaceId
+    vehicleId
+    direction
+    lineName
+    lineRef
+    serviceJourneyId
+    operator
+    mode
+    lastUpdated
+    expiration
+    speed
+    heading
+    monitored
+    delay
+    location {
+      latitude
+      longitude
+    }
+  }
+`;
+
+const VEHICLES_QUERY = gql`
+  query VehiclesQuery {
+    vehicles {
+      ...VehicleFragment
+    }
+  }
+  ${VEHICLE_FRAGMET}
+`;
 
 const VEHICLE_UPDATES_SUBSCRIPTION = gql`
   subscription VehicleUpdates {
     vehicleUpdates {
-      codespaceId
-      vehicleId
-      direction
-      lineName
-      lineRef
-      serviceJourneyId
-      operator
-      mode
-      lastUpdated
-      expiration
-      speed
-      heading
-      monitored
-      delay
-      location {
-        latitude
-        longitude
-      }
+      ...VehicleFragment
     }
   }
+  ${VEHICLE_FRAGMET}
 `;
 
 type State = any;
@@ -33,19 +49,33 @@ type Action = any;
 
 const reducer = (state: State, action: Action) => {
   const payload = action.payload;
-  state[payload.vehicleUpdates.vehicleId] = payload.vehicleUpdates;
+  payload.forEach((vehicle: any) => {
+    state[vehicle.vehicleId] = vehicle;
+  })
+
   return state;
 }
 
 function App() {
   const [vehicles, dispatch] = useReducer(reducer, {});
+
+  const {
+    data: hydrationData
+  } = useQuery(VEHICLES_QUERY);
+
   const {
     data,
   } = useSubscription(VEHICLE_UPDATES_SUBSCRIPTION);
 
   useEffect(() => {
+    if (hydrationData && hydrationData.vehicles) {
+      dispatch({ payload: hydrationData.vehicles });
+    }
+  }, [hydrationData]);
+
+  useEffect(() => {
     if (data && data.vehicleUpdates) {
-      dispatch({ payload: data });
+      dispatch({ payload: [data.vehicleUpdates] });
     }
   }, [data]);
 
