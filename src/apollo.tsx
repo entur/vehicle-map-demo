42938e4aa71ec71bdbb61901decdd76a72a48f5c
Tom Erik Støwer <testower@gmail.com>
2020-12-07 21:00:25 +0100

    Extract apollo setup

---
+++ src/apollo.tsx
@@ -0,0 +1,31 @@
+import { split, HttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_REALTIME_GRAPHQL_ENDPOINT
+});
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_REALTIME_SUBSCRIPTIONS_ENDPOINT || '',
+  options: {
+    reconnect: true
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache()
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from '@apollo/client';
+
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { client } from './apollo';
 
-import { split, HttpLink,ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-
-const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_REALTIME_GRAPHQL_ENDPOINT
-});
-
-const wsLink = new WebSocketLink({
-  uri: process.env.REACT_APP_REALTIME_SUBSCRIPTIONS_ENDPOINT || '',
-  options: {
-    reconnect: true
-  }
-});
-
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink,
-);
-
-const client = new ApolloClient({
-  link: splitLink,
-  cache: new InMemoryCache()
-});
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
