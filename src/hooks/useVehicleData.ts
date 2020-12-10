ce04cdf5c893ae9a3a9b6a26fc1d474811b2ac59
Tom Erik Støwer <testower@gmail.com>
2020-12-10 12:31:00 +0100

    Use apollo client directly to avoid re-render on every subscription update

--- src/hooks/useVehicleData.ts
+++ src/hooks/useVehicleData.ts
@@ -1,35 +1,52 @@
-import { useQuery, useSubscription } from '@apollo/client';
+import { FetchResult, useApolloClient } from '@apollo/client';
 import { VEHICLES_QUERY, VEHICLE_UPDATES_SUBSCRIPTION } from 'api/graphql';
+import { Vehicle } from 'model/vehicle';
 import { useEffect } from "react";
 import useVehicleReducer, { ActionType } from './useVehicleReducer';
 
+const DEFAULT_FETCH_POLICY = 'no-cache';
+const DEFAULT_UPDATE_INTERVAL_IN_MS = 250;
+const DEFAULT_SWIPE_INTERVAL_IN_MS = 1000;
+
 export default function useVehicleData() {
   const [vehicles, dispatch] = useVehicleReducer();
+  const client = useApolloClient();
 
-  const {
-    data: hydrationData,
-  } = useQuery(VEHICLES_QUERY);
-
-  const {
-    data: subscriptionData,
-  } = useSubscription(VEHICLE_UPDATES_SUBSCRIPTION);
-
-  useEffect(() => {
-    if (hydrationData && hydrationData.vehicles) {
-      dispatch({ type: ActionType.HYDRATE, payload: hydrationData.vehicles });
+  useEffect(() => {
+    async function hydrate() {
+      const { data: hydrationData } = await client.query({
+        query: VEHICLES_QUERY,
+        fetchPolicy: DEFAULT_FETCH_POLICY,
+      });
+      if (hydrationData && hydrationData.vehicles) {
+        dispatch({ type: ActionType.HYDRATE, payload: hydrationData.vehicles });
+      }
     }
-  }, [hydrationData, dispatch]);
+    hydrate();
+  }, [client, dispatch]);
 
-  useEffect(() => {
-    if (subscriptionData && subscriptionData.vehicleUpdates) {
-      dispatch({ type: ActionType.UPDATE, payload: [subscriptionData.vehicleUpdates] });
+  useEffect(() => {
+    const buffer: Vehicle[] = [];
+    client.subscribe({
+      query: VEHICLE_UPDATES_SUBSCRIPTION,
+      fetchPolicy: DEFAULT_FETCH_POLICY
+    }).subscribe((fetchResult: FetchResult) => {
+      buffer.push(fetchResult?.data?.vehicleUpdates as Vehicle);
+    });
+
+    const timer = setInterval(() => {
+      dispatch({ type: ActionType.UPDATE, payload: buffer.splice(0, buffer.length) });
+    }, DEFAULT_UPDATE_INTERVAL_IN_MS);
+
+    return () => {
+      clearInterval(timer);
     }
-  }, [subscriptionData, dispatch]);
+  }, [client, dispatch]);
 
   useEffect(() => {
     const timer = setInterval(() => {
       dispatch({ type: ActionType.EXPIRE });
-    }, 5000);
+    }, DEFAULT_SWIPE_INTERVAL_IN_MS);
 
     return () => {
       clearInterval(timer);
