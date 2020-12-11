9030900ea5bfbf3bbe631d8d29d5a158bb5cca6e
Tom Erik Støwer <testower@gmail.com>
2020-12-11 14:43:22 +0100

    Fix map style to adjust width according to left positioning

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -56,7 +56,7 @@ export const Map = ({ data }: any) => {
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
-      style={{ left: "400px" }}
+      style={{ left: "400px", width: "calc(100% - 400px)" }}
     >
       {popupInfo && popupInfo.location && (
         <Popup
