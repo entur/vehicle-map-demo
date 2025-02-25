d8960ac26c862fb0b4ccae5db023960a9cd63d0c
a-limyr <andreas.limyr@entur.org>
2025-02-25 10:23:46 +0100

    Refactoring theme out of App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,14 @@
+// App.tsx
 import { useState } from "react";
 import { Filter } from "../types.ts";
 import { useVehiclePositionsData } from "../hooks/useVehiclePositionsData.ts";
 import { MapView } from "./MapView.tsx";
 import { ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
+import { theme } from "./theme.ts"; // Adjust the path if needed
 
 function App() {
   const [currentFilter, setCurrentFilter] = useState<Filter | null>(null);
   const data = useVehiclePositionsData(currentFilter);
-
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: "#afe8e5",
-        main: "#1fcac1",
-        dark: "#009081",
-        contrastText: "#fff",
-      },
-      secondary: {
-        light: "#e9d3ed",
-        main: "#9353a1",
-        dark: "#480063",
-        contrastText: "#000",
-      },
-    },
-  });
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
---
+++ src/components/theme.ts
@@ -0,0 +1,19 @@
+// theme.ts
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#afe8e5",
+      main: "#1fcac1",
+      dark: "#009081",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#e9d3ed",
+      main: "#9353a1",
+      dark: "#480063",
+      contrastText: "#000",
+    },
+  },
+});
